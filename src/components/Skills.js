3e964cea61c8173185c4e939975df794bb3e4d8a
Amineenima <aminemaourid1995@gmail.com>
2023-02-21 01:20:14 +0100

    adding some interactivity to skills while hovering over

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import './SkillsStyle.css';
+import { motion } from 'framer-motion';
 import php from '../assets/skills/php.png'
 import laravel from '../assets/skills/laravel.png'
 import react from '../assets/skills/react.png';
@@ -19,94 +20,106 @@ export default function Skills() {
     <div className='skills'>
         <h2>Skills and Tools</h2>
         <div className='skills-container'>
-            <div className='skill'>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}
+            >
                 <div className='logo-container'>
                     <img src={php} alt="php mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>PHP</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={laravel} alt="laravel mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Laravel</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={react} alt="react mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>React</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={js} alt="javascript mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Javascript</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={docker} alt="docker mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Docker</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={html} alt="html mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>HTML 5</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={css} alt="css mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>CSS 3</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={bootstrap} alt="bootstrap mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Bootstrap</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={angular} alt="angular mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Angular</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={swift} alt="swift mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Swift</h4>
-            </div>
-            <div className='skill'>
+            </motion.div>
+            <motion.div className='skill'
+            whileHover={{scale : 1.1}}>
                 <div className='logo-container'>
                     <img src={node} alt="node mascot" 
                     className='logo'/>
                 </div>
                 <span className='bar'></span>
                 <h4>Node Js</h4>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
